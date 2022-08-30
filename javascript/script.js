5e0364297c92229103b47b365dc2cc1b180b4d20
Omkar <omkarthapa0002@gmail.com>
2022-08-30 17:43:09 +0530

    .

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,4 @@
+//Responsive Nav-bar 
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -6,3 +7,17 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+  //Progress bar
+  window.onscroll = function() {myFunction1()};
+
+function myFunction1() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+// Theme Change
+function theme() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
